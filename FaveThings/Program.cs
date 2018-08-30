317f7bf54396a6d82eae1f41ff6b685638624252
Nathan Pabst <nathan.pabst@gmail.com>
2018-08-29 19:31:18 -0500

    Add project files.

---
+++ FaveThings/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FaveThings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
